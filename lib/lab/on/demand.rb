5b785fb3d078cc99d8393eb004199129b2625663
BRIDGE2THINK TEAM <team@bridge2think.com>
2018-05-28 15:59:08 +0200

    first commit

---
+++ lib/lab/on/demand.rb
@@ -0,0 +1,9 @@
+require "lab/on/demand/version"
+
+module Lab
+  module On
+    module Demand
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/lab/on/demand/version.rb
@@ -0,0 +1,7 @@
+module Lab
+  module On
+    module Demand
+      VERSION = "0.1.0"
+    end
+  end
+end
